aa0acf51de72af4238adc2153f41bccb6f2907f9
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-21 22:55:41 +0200

    Add initial project files

---
+++ backend/backend/utils.py
@@ -0,0 +1,7 @@
+
+
+def get_attr_in_lang(obj, request, attr_ar, attr_en) -> str:
+    try:
+        return getattr(obj, attr_ar) if request.headers.get('Accept-Language', 'en') == 'ar' else getattr(obj, attr_en)
+    except AttributeError:
+        return ''
