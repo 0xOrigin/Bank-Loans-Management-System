8eab839765c9561e872c672b949e8ef3e4be9cd2
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-24 23:13:38 +0200

    Fix issue in banks admin

--- backend/banks/admin.py
+++ backend/banks/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from core.admin import BankAdmin
+from core.admin import BaseBankAdmin
 from banks.models import Bank, Branch
 
 
 @admin.register(Bank)
-class BankAdmin(BankAdmin):
-    list_display = BankAdmin.list_display + ['name_en', 'name_ar']
+class BankAdmin(BaseBankAdmin):
+    list_display = BaseBankAdmin.list_display + ['name_en', 'name_ar']
     search_fields = ['name_en', 'name_ar',]
 
 
 @admin.register(Branch)
-class BranchAdmin(BankAdmin):
-    list_display = BankAdmin.list_display + ['name_en', 'name_ar', 'code', 'bank']
+class BranchAdmin(BaseBankAdmin):
+    list_display = BaseBankAdmin.list_display + ['name_en', 'name_ar', 'code', 'bank']
     search_fields = ['name_en', 'name_ar', 'code',]
     list_filter = ['bank']
