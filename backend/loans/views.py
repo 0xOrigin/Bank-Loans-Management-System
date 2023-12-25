a44c0ca0fe95ca3ad43ffaf0ca42eabde4c5e183
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-25 18:31:37 +0200

    Add initial loans views

--- backend/loans/views.py
+++ backend/loans/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, generics
+from rest_framework.response import Response
+from core.views import BaseBankViewSet
+from loans.serializers import LoanPlanSerializer, LoanSerializer, LoanPaymentSerializer
+from loans.models import LoanPlan, Loan, LoanPayment
 
-# Create your views here.
+
+class LoanPlanViewSet(BaseBankViewSet):
+    model = LoanPlan
+    queryset = model.objects.all()
+    serializer_class = LoanPlanSerializer
+
+
+class LoanViewSet(BaseBankViewSet):
+    model = Loan
+    queryset = model.objects.all()
+    serializer_class = LoanSerializer
+
+
+class LoanPaymentViewSet(BaseBankViewSet):
+    model = LoanPayment
+    queryset = model.objects.all()
+    serializer_class = LoanPaymentSerializer
