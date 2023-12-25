480bcc57e64f514c6262f87247d0913c3b23637b
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-25 18:31:20 +0200

    Alter loans admin

--- backend/loans/admin.py
+++ backend/loans/admin.py
@@ -5,15 +5,16 @@ from loans.models import LoanPlan, Loan, LoanPayment
 
 @admin.register(LoanPlan)
 class LoanPlanAdmin(BaseBankAdmin):
-    list_display = BaseBankAdmin.list_display
+    list_display = BaseBankAdmin.list_display + ['bank',]
     search_fields = ['annual_interest_rate', 'duration_in_months']
+    list_filter = ['bank',]
 
 
 @admin.register(Loan)
 class LoanAdmin(BaseBankAdmin):
     list_display = BaseBankAdmin.list_display + ['purpose', 'amount', 'plan', 'status', 'is_active', 'is_amortized']
     search_fields = ['purpose', 'amount', 'plan', 'customer', 'status', 'is_active', 'is_amortized', 'total_payable_amount', 'monthly_payable_amount', 'approved_at', 'released_at']
-    list_filter = ['plan', 'status', 'is_active', 'is_amortized', 'approved_at', 'released_at']
+    list_filter = ['bank', 'plan', 'status', 'is_active', 'is_amortized', 'approved_at', 'released_at']
 
 
 @admin.register(LoanPayment)
