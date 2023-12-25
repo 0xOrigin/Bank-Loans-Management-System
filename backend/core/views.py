05c1137071b08bcf74b054e32d4f51ffb7d4ef43
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-25 18:11:40 +0200

    Add NonCreatableViewSet to core app

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
 from rest_framework.renderers import BrowsableAPIRenderer
 from django_filters.rest_framework import DjangoFilterBackend
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from core.paginations import BankPagination
+from core.permissions import BaseBankPermissions
 from core.renderers import BankJSONRenderer
 
 
@@ -12,6 +14,7 @@ class BaseBankViewSet(viewsets.ModelViewSet):
     model = None
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     pagination_class = BankPagination
+    permission_classes = [BaseBankPermissions,]
     renderer_classes = [BankJSONRenderer, BrowsableAPIRenderer]
 
     def get_queryset(self):
@@ -29,6 +32,12 @@ class BaseBankViewSet(viewsets.ModelViewSet):
         instance.deleted_by = self.request.user
         instance.deleted_at = timezone.now()
         instance.save(update_fields=['deleted_by', 'deleted_at'])
+
+
+class NonCreatableViewSet(viewsets.ModelViewSet):
+    
+    def create(self, request, *args, **kwargs):
+        return Response({'detail': _('Method Not Allowed')}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class NonUpdatableViewSet(viewsets.ModelViewSet):
