cab349bf4a2941b5a1a2e1c0476593c5e90a21f7
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-25 18:30:48 +0200

    Add initial loans serializers

--- backend/loans/serializers.py
+++ backend/loans/serializers.py
@@ -1,1 +1,24 @@
 from rest_framework import serializers
+from core.serializers import BaseBankSerializer
+from loans.models import LoanPlan, Loan, LoanPayment
+
+
+class LoanPlanSerializer(BaseBankSerializer):
+
+    class Meta:
+        model = LoanPlan
+        exclude = BaseBankSerializer.Meta.exclude + ('bank',)
+
+
+class LoanSerializer(BaseBankSerializer):
+
+    class Meta:
+        model = Loan
+        exclude = BaseBankSerializer.Meta.exclude
+
+
+class LoanPaymentSerializer(BaseBankSerializer):
+
+    class Meta:
+        model = LoanPayment
+        exclude = BaseBankSerializer.Meta.exclude
