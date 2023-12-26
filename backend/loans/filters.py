42d612b576dc626621d1304513f9092c55d4c983
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-12-26 19:46:30 +0200

    Add Loan filters

--- backend/loans/filters.py
+++ backend/loans/filters.py
@@ -1,5 +1,22 @@
 from django_filters.rest_framework import FilterSet, filters
-from loans.models import LoanPayment
+from loans.models import Loan, LoanPayment
+
+
+
+class LoanFilter(FilterSet):
+
+    class Meta:
+        model = Loan
+        fields = {
+            'purpose': ['exact', 'icontains'],
+            'amount': ['exact', 'gte', 'lte'],
+            'plan': ['exact',],
+            'status': ['exact',],
+            'is_active': ['exact',],
+            'is_amortized': ['exact',],
+            'approved_at': ['exact', 'gte', 'lte'],
+            'disbursed_at': ['exact', 'gte', 'lte'],
+        }
 
 
 class LoanPaymentFilter(FilterSet):
